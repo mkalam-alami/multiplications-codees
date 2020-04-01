e8e7bcbc6d9215532ff0e30ac056ad0c726d1dba
Marwane <marwane.ka@gmail.com>
2020-04-01 12:17:56 +0200

    Basic multiplication table

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,9 +1,14 @@
 import log from "shared/core/log";
 import Vue from "vue";
 
+const GRID_SIZE = 12
+
 new Vue({
   el: '#app',
   data: {
+    constants: {
+      GRID_SIZE
+    },
     name: 'les gens'
   },
   created() {
